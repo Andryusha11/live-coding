7f676f23757d4b21d3caa6f6cc7f44ecf2e7645a
Andrew Valchuk <andryushahach1999@gmail.com>
2022-10-26 11:25:45 +0300

    live coding

---
+++ 26-10-22-js-function/js/index.js
@@ -0,0 +1,62 @@
+// function getSensOfLife() {
+//   return 42;
+// }
+
+// const res = getSensOfLife();
+
+// console.log(res);
+// console.log(getSensOfLife());
+
+// const getSquare = (num) => num * num;
+
+// function getSquared(num) {
+//   return num * num;
+// }
+
+// console.log(getSquared(10));
+// console.log(getSquared(-9));
+// console.log(getSquared(0));
+// console.log(getSquared('t'));
+// console.log(getSquared(undefined));
+
+// function sum(num1, num2) {
+//   return num1 + num2;
+// }
+// console.log(sum(10, 1));
+// console.log(sum(-9, 2));
+// console.log(sum(0, 0));
+// console.log(sum('t', 5));
+// console.log(sum(undefined, 5));
+// console.log(sum(null, 5));
+
+// function getMessage(age) {
+//   if (age < 0 || typeof age !== 'number') {
+//     return null;
+//   }
+//   console.log('I am ' + age + ' years old!');
+// }
+
+// console.log(getMessage(-9));
+// console.log(getMessage(10));
+// console.log(getMessage(undefined));
+// console.log(getMessage(null));
+// console.log(getMessage());
+
+// function mult(age) {
+//   console.log('I am ' + age + ' years old!');
+// }
+
+// const mult = (a, b) => a * b;
+
+// console.log(mult(2, 3));
+// console.log(mult(0, 3));
+// console.log(mult(-2, -3));
+// console.log(mult(null, 3));
+// console.log(mult(undefined, 3));
+
+// const mult = () => 17;
+
+// console.log(mult(2));
+// console.log(mult('t'));
+// console.log(mult(null));
+// console.log(mult(undefined));
